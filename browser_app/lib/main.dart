fc3b58a5e6660a8865633f95f9f828ad810635a8
Gautam <61959145+gautam7-github@users.noreply.github.com>
2021-06-18 14:16:15 +0530

    Name Updated

--- browser_app/lib/main.dart
+++ browser_app/lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "PrivaBrowse",
       theme: ThemeData.dark().copyWith(
         accentColor: Colors.purple,
         scaffoldBackgroundColor: Colors.white,
